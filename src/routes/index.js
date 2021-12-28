90d7ee9f99ac9eb445e222c122ff162fb53afff8
FIGUREX <kennethggm12@gmail.com>
2021-12-27 21:38:08 -0600

    Implementar y probar las conexiones

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,11 @@ const router = async () => {
 	const content = null || document.getElementById('content');
 
 	header.innerHTML = await Header();
+	let hash = getHash();
+	let route = await resolveRoutes(hash);
+	let render = routes[route] ? routes[route] : Error404;
+	console.log(routes[route]);
+	content.innerHTML = await render();
 };
 
 export default router;
